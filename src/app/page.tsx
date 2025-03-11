b00563c2a99333d5aa8f4b8e733c45bd31624259
0xCryptoAngel <0xcryptoangel@gmail.com>
2025-03-10 21:44:29 -0700

    update ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,20 @@
-"use client"; // This directive tells Next.js that this file is for client-side rendering
+"use client";
 
-// Importing components and hooks from other files or libraries
-import { SignInAuthButton } from "@/components/signin-auth-button"; // Button for signing in with authentication
-import { Typography } from "@/components/typography"; // Custom typography component
-import Link from "next/link"; // Next.js Link component for navigation
-import { useState } from "react"; // React hook for managing state
-
-// Importing useWallet hook from the Crossmint SDK for wallet management
+import { SignInAuthButton } from "@/components/signin-auth-button";
+import { Typography } from "@/components/typography";
+import { useRouter } from "next/navigation";
 import { useWallet } from "@crossmint/client-sdk-react-ui";
-import { Button } from "@/components/button";
-
-// Main component for handling the primary action (minting NFT or signing in)
-function HomePrimaryAction() {
-  const { status: walletStatus } = useWallet(); // Destructuring wallet status from the useWallet hook
-
-  // If wallet is not loaded (user not signed in), show the sign-in button
-  if (walletStatus !== "loaded") {
-    return <SignInAuthButton />; // Button for signing in with a wallet
-  } else {
-    return (
-      <Button
-        className="bg-card gap-[10px] shadow-light rounded-xl py-3" // Styling for the button
-      >
-        <Link href="/wallet">
-          <Typography className="text-[#00150D] font-semibold text-[17px]">
-            Collection
-          </Typography>
-        </Link>
-      </Button>
-    );
-  }
-}
-
-// Main Home component to render the page
 export default function Home() {
+  const router = useRouter();
+  const { status: walletStatus } = useWallet();
+  if (walletStatus === "loaded") {
+    router.push("/wallet");
+  }
   return (
     <div className="flex h-full w-full items-center md:p-4 justify-center">
-      {/* Main container div, centered horizontally and vertically */}
       <div className="flex flex-col pb-12 items-center max-w-[538px] p-4">
-        {/* Content container for the entire page */}
         <div className="flex flex-col gap-2 text-center pb-8">
-          {/* Title and description section */}
-          <Typography
-            variant={"h1"} // Variant for the Typography component (heading level 1)
-          >
-            TEKORA CLUB
-          </Typography>
-          {/* Subtitle or description */}
+          <Typography variant={"h1"}>TEKORA CLUB</Typography>
           <Typography className="text-center">
             The Tekora Wallet is your secure hub for storing, managing, and
             trading exclusive Tekora NFTs. Unlock limited-edition digital cards,
@@ -54,29 +22,22 @@ export default function Home() {
             members. Take control of your collection, shape the future, and
             master the world of Tekora.
           </Typography>
-          <Typography
-            className="text-center"
-            variant={"h6"} // Variant for the Typography component (heading level 1)
-          >
+          <Typography className="text-center" variant={"h6"}>
             For the best experience, use a mobile browser.
           </Typography>
         </div>
 
-        {/* Main content with NFT details and primary action buttons */}
         <div className="flex flex-col w-full md:max-w-[340px] gap-10">
-          {/* NFT Information Card */}
           <div className="bg-card flex flex-col p-5 rounded-3xl shadow-dropdown">
-            {/* NFT image with styling */}
             <img
               className="rounded-xl rounded-bl-none rounded-br-none"
               src={
                 "https://playtekora.com/wp-content/uploads/2025/03/Wallet-Front-.png"
-              } // Path to the NFT image
-              alt="nft" // Alt text for accessibility
+              }
+              alt="nft"
             />
           </div>
-          {/* Render the HomePrimaryAction component to handle the wallet and minting logic */}
-          <HomePrimaryAction />
+          <SignInAuthButton />
         </div>
       </div>
     </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,28 +1,22 @@
-"use client"; // This directive tells Next.js that this file is to be client-side rendered
+"use client";
 
-// Importing various components, icons, and hooks
-import { Logo, MobileLogo } from "@/icons/logo"; // Logo components (custom icons)
-import { LogoutIcon } from "@/icons/logout"; // Logout icon
-import { Copy, Image as ImageIcon, User, WalletMinimal } from "lucide-react"; // Importing icons from the lucide-react library
-import Link from "next/link"; // Next.js Link component for navigation
-import { useRouter } from "next/navigation"; // Router hook to programmatically navigate
-
-// Importing client-side hooks and context
+import { Copy, Image, Globe, LogOut, User, WalletMinimal } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import {
   type EVMSmartWallet,
   useAuth,
   useWallet,
-} from "@crossmint/client-sdk-react-ui"; // Auth and Wallet hooks
-import { Avatar, AvatarFallback, AvatarImage } from "./avatar"; // Avatar components for user profile picture
+} from "@crossmint/client-sdk-react-ui";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuTrigger,
-} from "./dropdown-menu"; // Dropdown menu components
-import { Typography } from "./typography"; // Typography component for consistent text styling
-import { useToast } from "./use-toast"; // Custom hook for showing toast notifications
+} from "./dropdown-menu";
+import { Typography } from "./typography";
+import { useToast } from "./use-toast";
 
-// Function to format the wallet address (e.g., "0x123...abcd")
 function formatWalletAddress(
   address: string,
   startLength: number,
@@ -33,32 +27,27 @@ function formatWalletAddress(
   )}`;
 }
 
-// Main Header component
 export const Header: React.FC = () => {
-  const { logout } = useAuth(); // Extracting logout function from the useAuth hook
-  const { wallet, status: walletStatus } = useWallet(); // Extracting wallet details and status from the useWallet hook
-  const router = useRouter(); // Getting the router object for navigation
-  const { toast } = useToast(); // Getting the toast function for showing notifications
+  const { logout } = useAuth();
+  const { wallet, status: walletStatus } = useWallet();
+  const router = useRouter();
+  const { toast } = useToast();
 
-  // Handle logout and redirect the user to the homepage
   const handleLogout = () => {
-    logout(); // Perform logout action
-    router.push("/"); // Redirect to the homepage after logout
+    logout();
+    router.push("/");
   };
 
-  // Handle copying the wallet address to the clipboard
   const handleCopyAddress = async () => {
     if (wallet?.address) {
-      await navigator.clipboard.writeText(wallet.address); // Copy the wallet address to clipboard
-      toast({ title: "Address copied to clipboard", duration: 5000 }); // Show toast notification
+      await navigator.clipboard.writeText(wallet.address);
+      toast({ title: "Address copied to clipboard", duration: 5000 });
     }
   };
 
   return (
     <div className="flex justify-between p-4 items-center">
-      {/* Render the logo */}
       <HeaderLogo />
-      {/* Render the user menu if the wallet is loaded or in progress */}
       {(walletStatus === "loaded" || walletStatus === "in-progress") && (
         <UserMenu
           wallet={wallet}
@@ -71,7 +60,6 @@ export const Header: React.FC = () => {
   );
 };
 
-// Header logo component
 const HeaderLogo: React.FC = () => (
   <Link href="/" className="justify-center items-center flex">
     <div className="w-44 sm:w-64">
@@ -79,14 +67,13 @@ const HeaderLogo: React.FC = () => (
         className=""
         src={
           "https://playtekora.com/wp-content/uploads/2025/03/tekora-wallet-logo.png"
-        } // Path to the NFT image
-        alt="nft" // Alt text for accessibility
+        }
+        alt="nft"
       />
     </div>
   </Link>
 );
 
-// User menu component with wallet-related options
 const UserMenu: React.FC<{
   wallet: EVMSmartWallet | undefined;
   walletStatus: string;
@@ -95,29 +82,25 @@ const UserMenu: React.FC<{
 }> = ({ wallet, walletStatus, onLogout, onCopyAddress }) => (
   <DropdownMenu>
     <DropdownMenuTrigger asChild disabled={walletStatus !== "loaded"}>
-      {/* Trigger for the dropdown, disabled if wallet is not loaded */}
+      Trigger for the dropdown, disabled if wallet is not loaded
       <div className="flex items-center gap-5 cursor-pointer">
-        {/* Display wallet address and avatar */}
         <WalletDisplay
           address={wallet?.address}
           isLoading={walletStatus !== "loaded"}
         />
         <Avatar className="h-9 w-9">
           <AvatarImage alt="User Avatar" src="" />{" "}
-          {/* Empty source here, can be replaced with actual user image */}
           <AvatarFallback className="bg-skeleton">
-            <User className="h-5 w-5" /> {/* Default user icon */}
+            <User className="h-5 w-5" />
           </AvatarFallback>
         </Avatar>
       </div>
     </DropdownMenuTrigger>
-    {/* Dropdown content with options */}
     <DropdownMenuContent
       align="end"
       className="w-56 overflow-y-auto max-h-[80vh]"
     >
       <div className="flex flex-col gap-2">
-        {/* Copy wallet address to clipboard */}
         <div
           className="flex gap-3 text-muted items-center cursor-pointer py-2"
           onClick={onCopyAddress}
@@ -125,37 +108,35 @@ const UserMenu: React.FC<{
           <Typography>
             {wallet ? formatWalletAddress(wallet.address, 14, 6) : ""}
           </Typography>
-          <Copy className="h-5 w-5" /> {/* Copy icon */}
+          <Copy className="h-5 w-5" />
         </div>
-        {/* Link to Collection page */}
-        <Link
-          href="/wallet"
-          prefetch={false}
-          className="text-secondary-foreground flex gap-3 py-2"
-        >
-          <ImageIcon className="h-5 w-5" /> {/* Image icon for Collection */}
+        <Link href="/wallet" prefetch={false} className="flex gap-3 py-2">
+          <Image />
           <Typography>Collection</Typography>
         </Link>
-        {/* Logout option */}
-        <div
-          className="text-secondary-foreground flex gap-3 py-2 cursor-pointer"
-          onClick={onLogout}
+        <Link
+          href="https://playtekora.com/"
+          className=" flex gap-3 py-2"
+          passHref
         >
-          <LogoutIcon className="h-5 w-5" /> {/* Logout icon */}
-          <Typography>Logout</Typography>
+          <Globe />
+          <Typography>Go To</Typography>
+        </Link>
+        <div className=" flex gap-3 py-2 cursor-pointer" onClick={onLogout}>
+          <LogOut />
+          <Typography>Sign Out</Typography>
         </div>
       </div>
     </DropdownMenuContent>
   </DropdownMenu>
 );
 
-// Wallet display component to show wallet address and a loading state
 const WalletDisplay: React.FC<{
   address: string | undefined;
   isLoading: boolean;
 }> = ({ address, isLoading }) => (
   <div className="flex items-center min-w-[150px] bg-skeleton rounded-full px-4 py-2 gap-2 text-secondary-foreground">
-    <WalletMinimal className="h-4 w-4" /> {/* Wallet icon */}
+    <WalletMinimal className="h-4 w-4" />
     <Typography>
       {isLoading
         ? "Loading..."
