1d1bae7f3c579f50e48dface70f48cf2f1d18ed3
0xCryptoAngel <0xcryptoangel@gmail.com>
2025-03-16 21:10:21 +0200

    update wallet and collection

--- src/app/wallet/page.tsx
+++ src/app/wallet/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import { Skeleton } from "@/components/skeleton";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/tabs";
-import { Typography } from "@/components/typography";
 import { useQuery } from "@tanstack/react-query";
-import { useWallet, useAuth  } from "@crossmint/client-sdk-react-ui";
+import { useWallet, useAuth } from "@crossmint/client-sdk-react-ui";
 import Link from "next/link";
 
 type NFT = {
@@ -27,7 +25,7 @@ const SkeletonLoader = () => {
   return (
     <div className="p-6 flex h-full w-full items-center  gap-6 justify-center flex-col">
       <div className="w-full flex-col sm:max-w-[418px] bg-card rounded-2xl shadow-dropdown min-h-[560px] p-6">
-               <div className="grid grid-cols-1 sm:grid-cols-2 justify-items-center gap-x-4 gap-y-8 pb-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 justify-items-center gap-x-4 gap-y-8 pb-6">
           <div className="w-full sm:w-auto flex flex-col gap-4">
             <Skeleton className="w-full h-40 sm:w-40 rounded-[10px]" />
             <Skeleton className="h-4 w-24" />
@@ -52,21 +50,24 @@ const SkeletonLoader = () => {
 
 export default function Index() {
   const { wallet, status: walletStatus } = useWallet();
-  const { user } = useAuth ();
-  const  shortenAddress = (address:string) => {
+  const { user } = useAuth();
+  const shortenAddress = (address: string) => {
     if (!address || address.length < 10) return address; // Handle invalid input
     return `${address.slice(0, 8)}...${address.slice(-6)}`;
-  }
-  const chain = "polygon"
-  const fetchNFTs = async (user:any) => {
+  };
+  const chain = "polygon";
+  const fetchNFTs = async (user: any) => {
     try {
-      const walletLocator = encodeURIComponent(`email:${(user?.email)}:${chain}`)
+      const walletLocator = encodeURIComponent(`email:${user?.email}:${chain}`);
       const response = await fetch(
         `https://www.crossmint.com/api/2022-06-09/wallets/${walletLocator}/nfts?page=1&perPage=20`,
         {
           method: "GET",
           headers: {
-            'X-API-KEY': `${process.env.NEXT_PUBLIC_CROSSMINT_AUTH_SMART_WALLET_SERVER_API_KEY ?? ""}`
+            "X-API-KEY": `${
+              process.env
+                .NEXT_PUBLIC_CROSSMINT_AUTH_SMART_WALLET_SERVER_API_KEY ?? ""
+            }`,
           },
         }
       );
@@ -74,8 +75,8 @@ export default function Index() {
       return data;
     } catch (err) {
       console.log(err.message);
-    } 
-  }
+    }
+  };
 
   const { data, isLoading: isLoadingNFTs } = useQuery({
     queryKey: ["smart-wallet"],
@@ -85,7 +86,6 @@ export default function Index() {
     staleTime: 1000 * 60 * 5, // 5 minutes
     enabled: wallet != null,
   });
-  
 
   if (walletStatus === "in-progress" || isLoadingNFTs) {
     return <SkeletonLoader />;
@@ -93,49 +93,42 @@ export default function Index() {
 
   return (
     <div className="p-6 flex h-full w-full items-center pt-6 gap-6 justify-center flex-col">
-      <div className="w-full flex-col sm:max-w-[418px] bg-card rounded-2xl shadow-dropdown min-h-[664px] p-6">
-        <Tabs defaultValue="collectibles" className="my-2">
-          <TabsList className="grid w-full">
-            <TabsTrigger value="collectibles">Collectibles</TabsTrigger>
-          </TabsList>
-          <TabsContent
-            value="collectibles"
-            className="h-[580px] overflow-y-auto"
-          >
-            {data?.length === 0 ? (
-              <Typography className="text-base text-primary-foreground p-4">
-                {"You have no collectibles."}
-              </Typography>
-            ) : (
-              <div className="grid grid-cols-1 sm:grid-cols-2 justify-items-center gap-x-4 gap-y-8 py-6">
-                {(data || []).map((nft) => (
-                  <Link
-                    href={`/wallet/${nft.locator}`}
-                    className="flex flex-col gap-4"
-                    key={nft.tokenId + nft.contractAddress}
-                  > 
-                    <img
-                      className="rounded-[10px] max-w-full sm:max-w-[164px] aspect-square overflow-hidden object-cover"
-                      src={nft.metadata.image}
-                      alt={nft.metadata.description}
-                    />
-                    <div className="flex flex-col">
-                      <Typography className="text-base text-color-secondary-foreground leading-none">
-                        {nft.metadata.name}
-                      </Typography>
-                      <Typography className="text-sm text-muted">
-                        {shortenAddress(nft.contractAddress)}
-                      </Typography>
+      <div className="w-full flex-col bg-card rounded-2xl shadow-dropdown min-h-[664px] p-6 md:max-w-[800px]">
+        <div className="border-b-2 pb-1 mb-2 w-full">
+          <div className="text-2xl text-center">Collectibles</div>
+        </div>
+        <div className="h-[580px] overflow-y-auto">
+          {data?.length === 0 ? (
+            <div className="text-base text-primary-foreground p-4">
+              {"You have no collectibles."}
+            </div>
+          ) : (
+            <div className="py-6">
+              {(data || []).map((nft) => (
+                <Link
+                  href={`/wallet/${nft.locator}`}
+                  className="flex flex-col gap-4"
+                  key={nft.tokenId + nft.contractAddress}
+                >
+                  <img
+                    className="rounded-[10px] max-w-full aspect-square overflow-hidden object-cover"
+                    src={nft.metadata.image}
+                    alt={nft.metadata.description}
+                  />
+                  <div className="flex flex-col">
+                    <div className="text-base text-color-secondary-foreground leading-none">
+                      {nft.metadata.name}
                     </div>
-                  </Link>
-                ))}
-              </div>
-            )}
-          </TabsContent>
-        </Tabs>
+                    <div className="text-sm text-muted">
+                      {shortenAddress(nft.contractAddress)}
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
 }
-
-
