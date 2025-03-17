810b43ad73529ded50a047590279405542b6c6ba
0xCryptoAngel <0xcryptoangel@gmail.com>
2025-03-17 05:11:56 +0200

    fix but

--- src/app/wallet/[locator]/page.tsx
+++ src/app/wallet/[locator]/page.tsx
@@ -5,6 +5,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useWallet, useAuth } from "@crossmint/client-sdk-react-ui";
 import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
+import { useEffect, useState } from "react";
 
 type NFT = {
   chain: string;
@@ -81,8 +82,10 @@ const Attribute = ({ value, label }: AttributeProps) => (
 );
 
 export default function Index({ params }: { params: { locator: string } }) {
-  const { wallet, status: walletStatus } = useWallet();
+  const { status: walletStatus } = useWallet();
+  const [nft, setNft] = useState<NFT>();
   const { user } = useAuth();
+  const [loading, setLoading] = useState(true);
   const chain = "polygon";
   const fetchNFTs = async (user: any) => {
     try {
@@ -107,22 +110,17 @@ export default function Index({ params }: { params: { locator: string } }) {
         "n.locator === decodeURIComponent(params.locator)",
         decodeURIComponent(params.locator)
       );
-      console.log("nft", nft);
-      return nft;
+      setLoading(false);
+      setNft(nft);
     } catch (err) {
       console.log(err.message);
     }
   };
-  const { data, isLoading: isLoadingNFTs } = useQuery({
-    queryKey: ["smart-wallet"],
-    queryFn: async () => (wallet != null ? await fetchNFTs(user) : {}) as NFT,
-    refetchOnWindowFocus: false,
-    refetchOnMount: false,
-    staleTime: 1000 * 60 * 5, // 5 minutes
-    enabled: wallet != null,
-  });
-  console.log("____________data_____________", data);
-  if (walletStatus === "in-progress" || isLoadingNFTs) {
+  useEffect(() => {
+    fetchNFTs(user);
+  }, []);
+
+  if (walletStatus === "in-progress" || loading) {
     return <SkeletonLoader />;
   }
   return (
@@ -138,21 +136,21 @@ export default function Index({ params }: { params: { locator: string } }) {
       </div>
       <div className="flex flex-col gap-4">
         {/* NFT Image */}
-        <img src={data?.metadata?.image} alt="image" className="rounded-xl" />
+        <img src={nft?.metadata?.image} alt="image" className="rounded-xl" />
         {/* NFT Details */}
         <div className="flex flex-col gap-6">
-          <div className="text-3xl font-bold">{data?.metadata?.name}</div>
+          <div className="text-3xl font-bold">{nft?.metadata?.name}</div>
           <div className="mb-8">
             <h2 className="text-xl font-semibold mb-2">Description</h2>
             <p className="text-gray-700 leading-relaxed">
-              {data?.metadata?.description}
+              {nft?.metadata?.description}
             </p>
           </div>
 
           <div className="mb-8">
             <h2 className="text-xl font-semibold mb-4">Attributes</h2>
             <div className="grid grid-cols-3 gap-3">
-              {data?.metadata?.attributes?.map((attr, Index) => {
+              {nft?.metadata?.attributes?.map((attr, Index) => {
                 return (
                   <Attribute
                     key={Index}
@@ -169,20 +167,20 @@ export default function Index({ params }: { params: { locator: string } }) {
               <div className="grid grid-cols-2 gap-4">
                 <span className="text-gray-700">Contract Address</span>
                 <span className="text-gray-900 font-mono w-40 sm:w-96 truncate sm:text-clip">
-                  {data?.contractAddress}
+                  {nft?.contractAddress}
                 </span>
               </div>
               <div className="grid grid-cols-2 gap-4">
                 <span className="text-gray-700">Token ID</span>
-                <span className="text-gray-900">{data?.tokenId}</span>
+                <span className="text-gray-900">{nft?.tokenId}</span>
               </div>
               <div className="grid grid-cols-2 gap-4">
                 <span className="text-gray-700">Blockchain</span>
-                <span className="text-gray-900">{data?.chain}</span>
+                <span className="text-gray-900">{nft?.chain}</span>
               </div>
               <div className="pt-8">
                 <a
-                  href={`https://polygonscan.com/token/${data?.contractAddress}?a=${data?.tokenId}`}
+                  href={`https://polygonscan.com/token/${nft?.contractAddress}?a=${nft?.tokenId}`}
                   className="text-blue-600 hover:underline"
                   target="_blank"
                   rel="noopener noreferrer"
